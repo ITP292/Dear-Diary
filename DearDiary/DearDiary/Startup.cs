823de19a90e2762a54057eba98f6ca5b88eb8d20
Ryan <ryanchew96@gmail.com>
2016-10-28 10:36:30 +0800

    Welcome to hell (Inital Checkin)

---
+++ DearDiary/DearDiary/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DearDiary.Startup))]
+namespace DearDiary
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
