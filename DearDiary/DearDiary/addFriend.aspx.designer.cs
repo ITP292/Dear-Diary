35d530f127f3eb71b9c9f16dfcd962e464851946
Ryan <ryanchew96@gmail.com>
2016-11-18 18:16:25 +0800

    Edited addfriend

--- DearDiary/DearDiary/addFriend.aspx.designer.cs
+++ DearDiary/DearDiary/addFriend.aspx.designer.cs
@@ -13,30 +13,30 @@ namespace DearDiary {
     public partial class addFriend {
         
         /// <summary>
-        /// Label1 control.
+        /// ErrorMessage control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.PlaceHolder ErrorMessage;
         
         /// <summary>
-        /// TextBox1 control.
+        /// FailureText control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.Literal FailureText;
         
         /// <summary>
-        /// Button1 control.
+        /// Email control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.TextBox Email;
     }
 }
